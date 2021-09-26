b17005ca1d90a8bc25b305ae3f1e8a9eb9e6f79a
Kinga Kasicka <kinga kasicka>
2021-09-26 18:42:06 +0100

    Szóste zadanie

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,15 @@ public class Main {
 
         /* zad1. stworzyć tablicę typu double 5-cio elementową,
         przypisać do niej wartości i wyświetlić */
-
+         int[]tab=new int[5];
+         for(int i=0;i<tab.length;i++)
+         {
+             tab[i]=i+2;
+         }
+         for(int i=0;i<tab.length;i++)
+         {
+             System.out.println(tab[i]+"");
+         }
+        
     }
 }
