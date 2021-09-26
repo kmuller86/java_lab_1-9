7856ccda85b0345cc11bf09da387597e43229180
Kinga Kasicka <kinga kasicka>
2021-09-26 10:55:19 +0100

    Czwarte zadanie

--- src/Main.java
+++ src/Main.java
@@ -7,9 +7,9 @@ public class Main {
         /* ćw.4 */
 
         /* zad.1 proszę dodać kilka złożonych operacji uzywając kilku operatorów arytmetycznych i logicznych */
-        int x=8;
-        int y=4;
-        int z=10;
+        int x = 8;
+        int y = 4;
+        int z = 10;
 
 
     }
