416207499621d0f41f21a46318073457d27d6ee7
Kinga Kasicka <kinga kasicka>
2021-09-26 18:05:51 +0100

    Piąte zadanie

--- src/Main.java
+++ src/Main.java
@@ -16,11 +16,27 @@ public class Main {
              System.out.println("Niepodzielny przez 3");
 
         /* zad.2 użyj parametru trójargumentowego aby sprawdzić parzystośc nr. indeksu */
-
-
+        int indeks=32975;
+        int wynik  = (indeks % 2 == 0) ? 0 : 1;
+        System.out.println(wynik);
 
         /* zad.3 wpisz z klawiatury liczbę zmiennoprzecinkową (użyj klasy Scanner z poprzednich ćwiczeń)
          * i stwórz konstrukcję else if z dowolnymi komunikatami */
+        System.out.println("Podaj liczbę zmiennoprzecinkową");
+        Scanner scan=new Scanner(System.in);
+        double liczba=scan.nextDouble();
+        if(liczba>5)
+        {
+            System.out.println("Do upieczenia ciasta potrzebujemy ponad 5 jajek");
+        }
+        else if(liczba<5)
+        {
+            System.out.println("Do upieczenia ciasta potrzebujemy mniej niż 5 jajek");
+        }
+        else if(liczba==5)
+        {
+            System.out.println("Do upieczenia ciasta potrzebujemy równo 5 jajek");
+        }
 
     }
 }
